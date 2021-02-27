52bbb787ca260eeb5c05b46bdca121969bbe9070
prahal <vprahaladhan@gmail.com>
2021-02-27 13:45:47 +1100

    Final commit

--- index.js
+++ index.js
@@ -1,30 +1,18 @@
 // Your code here
 const dodger = document.getElementById('dodger');
-// const game = document.getElementById('game');
-// const dodgerStyle = window.getComputedStyle(dodger, null);
-// const gameStyle = window.getComputedStyle(game, null);
-// const maxWidth = gameStyle.width.replace("px", "") - dodgerStyle.width.replace("px", "")
+const leftNumbers = dodger.style.left.replace("px", "");
+const left = parseInt(leftNumbers, 10);
 
-function moveDodgerLeft() {
-  const leftNumbers = dodger.style.left.replace("px", "");
-  const left = parseInt(leftNumbers, 10);
-
-  if (left > 0) {
-    dodger.style.left = `${left - 1}px`;
-  }
+const moveDodgerLeft = () => {
+  dodger.style.left = (left > 0) ? `${left - 1}px` : dodger.style.left;
 }
 
-function moveDodgerRight() {
-  const leftNumbers = dodger.style.left.replace("px", "");
-  const left = parseInt(leftNumbers, 10);
-
-  if (left < 360) {
-    dodger.style.left = `${left + 1}px`;
-  }
+const moveDodgerRight = () => {
+  dodger.style.left = (left < 360) ? `${left + 1}px` : dodger.style.left;
 }
 
-const checkScrollDirection = event => checkScrollDirectionIsUp(event) ? moveDodgerLeft() : moveDodgerRight(); 
+const checkScrollDirection = event => isScrollDirectionUp(event) ? moveDodgerLeft() : moveDodgerRight(); 
 
-const checkScrollDirectionIsUp = event => event.wheelDelta ? event.wheelDelta > 0 : event.deltaY < 0;
+const isScrollDirectionUp = event => event.wheelDelta ? event.wheelDelta > 0 : event.deltaY < 0;
 
 document.body.addEventListener('wheel', checkScrollDirection);
